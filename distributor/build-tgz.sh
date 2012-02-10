533ed5b3cd24606bab38f5cebaca3ae735020501
Noah Anderson <noah.anderson@ncf.edu>
2012-02-09 22:13:20 -0500

    Update build-tgz

--- distributor/build-tgz.sh
+++ distributor/build-tgz.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 #
 # build-tgz.sh - Assemble distribution of distclient without Perl.
 #
 # Used for Linux systems that have Perl already, especially EC2.
 #
-ARCHIVE=distclient.tar.gz
-tar --transform 's|^|distclient/|' --show-stored-names --owner=0 --group=0 \
-    -cvzf "$ARCHIVE" distclient.pl \
-    distclientcli.pl server.conf lib
-echo Created $ARCHIVE
+ARCHIVE=distclient.tar
+FLAGS="--show-stored-names --owner=0 --group=0"
+rm -f "$ARCHIVE" "$ARCHIVE".gz
+tar --transform 's|^|distclient/|' $FLAGS \
+    -cvf "$ARCHIVE" distclient.pl distclientcli.pl server.conf
+LIBDIR=`perl -le 'for(@INC,"./lib"){print if -f "\$_/HTTP/Async.pm"}'`
+[ -z "$LIBDIR" ] && echo Can not find HTTP::Async && exit 1
+tar --transform "s|^$LIBDIR|distclient/lib|" $FLAGS \
+    --append -vf "$ARCHIVE" $LIBDIR/HTTP/Async{.pm,}
+gzip -9 "$ARCHIVE"
+echo Created "$ARCHIVE".gz
